23847ccbb92b5c8f2cd93b11918f52ff25c9c8b7
hp <1079374315@qq.com>
2024-03-29 15:04:30 +0800

    fz_9,1

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //main1
-        //fz_10
+        //1
     }
 }
