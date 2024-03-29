2d04f2507d5092574e6e6da1df2714627bc960fe
hp <1079374315@qq.com>
2024-03-29 13:12:31 +0800

    main_3

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -15,9 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //main_1
-        //修改1
         //main_2
-        //修改2
-        //main_3
     }
 }
