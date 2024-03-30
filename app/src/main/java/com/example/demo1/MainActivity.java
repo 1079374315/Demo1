b752514e35d53ab1cd18d44c45abfddfd80c5492
hp <1079374315@qq.com>
2024-03-30 13:02:27 +0800

    main_8

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //f7
-        //main_7
+        //main_8
     }
 }
