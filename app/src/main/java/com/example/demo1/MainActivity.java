2247a610711faa2318e438e6a565c4c89811a7de
hp <1079374315@qq.com>
2024-04-03 14:47:40 +0800

    main_17.2

--- app/src/main/java/com/example/demo1/MainActivity.java
+++ app/src/main/java/com/example/demo1/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //main_17.1
+        //main_17.2
     }
 }
